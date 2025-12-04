9a3cd599ae205e15e4f4a57ded0be79778579409
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-12-04 11:48:10 +0530

    feat: Initialize basic Express.js backend project with TypeScript and pnpm.

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

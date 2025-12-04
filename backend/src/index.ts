7340679dc8105c1f4670b103c382ca5f676af525
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-12-04 12:11:52 +0530

    feat: setup drizzle orm and docker compose file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cookieParser from "cookie-parser";
-
-dotenv.config();
 
 const app = express();
 
